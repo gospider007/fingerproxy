36547fb4a4666faff400878bd051d7a835ce594e
gospider <2216403312@qq.com>
2025-09-02 15:00:35 +0800

    sync

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ for root, dirs, files in os.walk(dist_dir):
 
 setup(
     name='fingerproxy',
-    version='1.0.0',
+    version='1.0.1',
     packages=find_packages(),
     include_package_data=True,
     package_data={
