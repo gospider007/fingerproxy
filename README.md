6a8e61a18664155632b4e6f81e8e906afa9a777a
gospider <2216403312@qq.com>
2025-09-02 15:20:37 +0800

    sync

--- README.md
+++ README.md
@@ -58,11 +58,11 @@ It provides unified support for HTTP/1, HTTP/2, WebSockets protocols.</h2>
 ## Installation
 ### github
 ```bash
-pip install git+https://github.com/gospider007/fingerproxy.git
+pip install --force-reinstall git+https://github.com/gospider007/fingerproxy.git
 ```
 ### gitee
 ```bash
-pip install git+https://gitee.com/gospider007/fingerproxy.git
+pip install --force-reinstall git+https://gitee.com/gospider007/fingerproxy.git
 ```
 
 ## Run FingerProxy
--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from setuptools import setup, find_packages
+from setuptools.command.install import install
 import os
+import shutil
 
 # 自动收集 dist 目录下的二进制文件
 dist_dir = os.path.join(os.path.dirname(__file__), 'dist')
@@ -11,9 +13,22 @@ for root, dirs, files in os.walk(dist_dir):
         file_path = os.path.relpath(os.path.join(root, file), start='fingerproxy')
         package_data_files.append(file_path)
 
+
+class CustomInstall(install):
+    def run(self):
+        # 获取安装路径
+        install_path = os.path.join(self.install_lib, 'your_package_name', 'dist')
+
+        # 删除旧的 dist 文件夹
+        if os.path.exists(install_path):
+            print(f"Removing old dist folder: {install_path}")
+            shutil.rmtree(install_path)
+        # 执行默认安装流程
+        install.run(self)
+
 setup(
     name='fingerproxy',
-    version='1.0.1',
+    version='1.0.0',
     packages=find_packages(),
     include_package_data=True,
     package_data={
@@ -25,5 +40,10 @@ setup(
         ]
     },
     zip_safe=False,
-  
+    cmdclass={
+        'install': CustomInstall,
+    },
 )
+
+
+
